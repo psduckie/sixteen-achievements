a9e8476380e67c070141b0e4bb1d092b6732052d
PSDuckie <psduckie@yahoo.com>
2018-09-13 02:39:30 -0400

    Added a faction dropdown.

--- sixteen-achievements/src/app/app.module.ts
+++ sixteen-achievements/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MatSelectModule, MatSlideToggleModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ sixteen-achievements/src/app/character/character.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.css']
+})
+
+export class CharacterComponent implements OnInit {
+  factions: string[] = ['Exile', 'Dominion'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
