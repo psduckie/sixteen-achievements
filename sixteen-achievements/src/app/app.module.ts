4afdb3dc3d8d4fb64fd98aaa4a139282382e68d2
PSDuckie <psduckie@yahoo.com>
2018-09-13 08:52:33 -0400

    Added race and path dropdowns.
    Race dropdown will only show valid options.

--- sixteen-achievements/src/app/app.module.ts
+++ sixteen-achievements/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { MatSelectModule, MatSlideToggleModule, MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { CharacterComponent } from './character/character.component';
@@ -13,6 +14,7 @@ import { CharacterComponent } from './character/character.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpModule,
     MatSelectModule,
     MatSlideToggleModule,
     MatCardModule
--- sixteen-achievements/src/app/character/character.component.ts
+++ sixteen-achievements/src/app/character/character.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-character',
@@ -9,10 +10,73 @@ import { Component, OnInit } from '@angular/core';
 export class CharacterComponent implements OnInit {
   factions: string[] = ['Exile', 'Dominion'];
   classes: string[] = ['Warrior', 'Spellslinger', 'Medic', 'Stalker', 'Esper', 'Engineer'];
+  races: string[];
+  paths: string[] = ['Explorer', 'Scientist', 'Soldier', 'Settler'];
 
-  constructor() { }
+  selectedFaction: string;
+  selectedClass: string;
+  selectedRace: string;
+  selectedPath: string;
+
+  matrixURL = '../../assets/class-matrix.csv';
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
+  calculateRaces() {
+    console.log('Selected faction: ' + this.selectedFaction);
+    console.log('Selected class: ' + this.selectedClass);
+
+    if (this.selectedFaction === undefined || this.selectedClass === undefined) {
+      return;
+    }
+
+    this.http.get(this.matrixURL).subscribe(data => this.extractRaceData(data));
+  }
+
+  extractRaceData(res: Response) {
+    const csvData = res['_body'] || '';
+    const allTextLines = csvData.split(/\r\n|\n/);
+    let currentLine;
+
+    this.races = [];
+
+    let counter = 0;
+    do {
+      currentLine = allTextLines[counter].split(',');
+      if (currentLine[0] !== this.selectedClass) {
+        counter++;
+      }
+    } while (currentLine[0] !== this.selectedClass);
+
+    if (this.selectedFaction === 'Exile') {
+      if (currentLine[1] === 'Yes') {
+        this.races.push('Human');
+      }
+      if (currentLine[2] === 'Yes') {
+        this.races.push('Granok');
+      }
+      if (currentLine[3] === 'Yes') {
+        this.races.push('Aurin');
+      }
+      if (currentLine[4] === 'Yes') {
+        this.races.push('Mordesh');
+      }
+    } else {
+      if (currentLine[5] === 'Yes') {
+        this.races.push('Cassian');
+      }
+      if (currentLine[6] === 'Yes') {
+        this.races.push('Draken');
+      }
+      if (currentLine[7] === 'Yes') {
+        this.races.push('Mechari');
+      }
+      if (currentLine[8] === 'Yes') {
+        this.races.push('Chua');
+      }
+    }
+  }
 }
